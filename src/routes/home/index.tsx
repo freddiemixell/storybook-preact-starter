1e181c1d2040dc5626b02f1331776f1e2191c451
Freddie Mixell <fmixell@gmail.com>
2021-11-02 19:35:14 -0400

    use the button

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,11 +1,13 @@
 import { FunctionalComponent, h } from 'preact';
 import style from './style.css';
+import Button from '../../components/button';
 
 const Home: FunctionalComponent = () => {
     return (
         <div class={style.home}>
             <h1>Home</h1>
             <p>This is the Home component.</p>
+            <Button onClick={() => console.log("I have been clicked.")}>Click me!</Button>
         </div>
     );
 };
