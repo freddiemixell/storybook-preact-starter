20c5cca7cbf7869ae46e5e27ac423bab2ed4269f
Freddie Mixell <fmixell@gmail.com>
2021-11-02 19:32:38 -0400

    adding babel config for typescript

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,8 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  babel: async (options) => ({
+    ...options,
+    presets: [["@babel/typescript", { jsxPragma: "h" }]],
+  }),
+};
