af8c113a308089b585a1f2e566f493ad60121f66
Freddie Mixell <fmixell@gmail.com>
2021-11-02 20:43:17 -0400

    opacity controls and more examples

--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -13,6 +13,7 @@ export default {
 		backgroundColor: { name: 'background color', control: 'text' },
 		rounded: { name: 'rounded', control: 'boolean' },
 		onClick: { action: 'clicked', control: 'action' },
+		backgroundOpacity: { name: 'background opacity', control: 'range', min: 0, max: 100, step: 25},
 	},
 	args: {
 		children: 'Button label',
@@ -32,6 +33,20 @@ Primary.args = {
 export const Green = Template.bind({});
 Green.args = {
 	backgroundColor: 'green',
-	rounded: true,
+	rounded: false,
 	textColor: 'white',
 };
+
+export const RoundedButton = Template.bind({});
+RoundedButton.args = {
+	backgroundColor: 'blue',
+	rounded: true,
+}
+
+export const LightButton = Template.bind({});
+LightButton.args = {
+	backgroundColor: 'blue',
+	backgroundOpacity: 50,
+	rounded: false,
+	textColor: 'white',
+}
